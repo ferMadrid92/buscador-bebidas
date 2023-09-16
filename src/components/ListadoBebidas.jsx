e9265fc23eb6c7f5193eaccc191bc1a78b9ebbf5
Fernando <fer.madridb@gmail.com>
2023-09-15 20:43:38 -0500

    fixes

--- src/components/ListadoBebidas.jsx
+++ src/components/ListadoBebidas.jsx
@@ -1,17 +1,25 @@
+import { useState, useEffect } from "react"
 import { Row } from "react-bootstrap"
 import useBebidas from "../hooks/useBebidas"
 import Bebida from "./Bebida"
 import Spinner from "./Spinner"
 
-const ListadoBebidas = () => {
 
+const ListadoBebidas = () => {
+    const [firstLoad, setFirstLoad] = useState(true)
     const { bebidas, cargaSpinner } = useBebidas()
+
+    useEffect(() => {
+      if (cargaSpinner) {
+        setFirstLoad(false)
+      }
+    }, [cargaSpinner])
 
   return (
     <>
     {cargaSpinner && <Spinner />}
     <Row className="mt-5">
-    {cargaSpinner ? null : (bebidas.length === 0 ? <h3>No drinks found, try a different drink/category combination</h3> : bebidas.map(bebida => (
+    {cargaSpinner ? null : (bebidas.length === 0 && !firstLoad ? <h3>No drinks found, try a different drink/category combination</h3> : bebidas.map(bebida => (
     <Bebida
         key={bebida.idDrink}
         bebida={bebida}
